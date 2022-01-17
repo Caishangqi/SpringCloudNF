1615bdcc8669dcd954efd46acd5845825de7e678
Caishangqi <2470159730@qq.com>
2022-01-17 05:17:09 -0800

    Initialize Service03

--- sp02-itemservice/src/main/java/cn/tedu/sp02/controller/ItemController.java
+++ sp02-itemservice/src/main/java/cn/tedu/sp02/controller/ItemController.java
@@ -5,7 +5,6 @@ import cn.tedu.web.util.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-c  xs
 import java.util.List;
 import java.util.Random;
 
